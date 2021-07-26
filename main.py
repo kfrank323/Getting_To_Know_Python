21133fd8671d5228037869153f0193210c22e3bc
Kyle Frank <68864046+kfrank323@users.noreply.github.com>
2021-07-26 19:04:09 +0000

    Added another rectangle

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from turtle import Turtle, Screen
 t: Turtle = Turtle()
 s: Screen = Screen()
 
+#sideLength: int = float(input("Give me a sideLength: "))
+sideLength: int = s.numinput('Input Window', 'Give me a num: ')
+
 # Draw Square
-t.forward(150)
-t.right(90)
-t.forward(150)
-t.right(90)
-t.forward(150)
-t.right(90)
-t.forward(150)
-t.right(90)
+sideNum: int = 0
+
+while sideNum < 4:
+  t.forward(sideLength)
+  t.right(90)
+  sideNum += 1
+
+t.right(180)
+
+for side in range(4):
+  t.forward(sideLength)
+  t.left(90)
